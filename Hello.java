88c5c1266d0515c80660c19d12abfc46be4bea38
karan-ekal <premekal07@gmail.com>
2023-05-05 23:42:03 +0530

    New

--- Hello.java
+++ Hello.java
@@ -2,9 +2,8 @@ class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome_!!,I am Karan Ekal");
+        System.out.println("I am Karan Ekal");
 
-        System.out.println("Email: karanekal07@gmail.com");
 
     }
 
