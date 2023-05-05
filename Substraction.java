31736b8622312e5ff87d8e17520b1d804116fbe9
karan-ekal <premekal07@gmail.com>
2023-05-05 23:48:09 +0530

    Created

---
+++ Substraction.java
@@ -0,0 +1,10 @@
+class Substraction {
+    public static void main(String[] args) {
+        int a, b;
+        a = 26;
+        b = 10;
+        int Sub = a - b;
+        System.out.println("Substraction of a and b is:"+Sub);
+        System.out.println("---------End-----------");
+    }
+}
