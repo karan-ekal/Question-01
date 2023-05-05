9f319b1e0adaffd153d2c561a200019937e2d06c
karan-ekal <premekal07@gmail.com>
2023-05-05 18:20:51 +0530

    New 1 Commit

---
+++ Substraction.java
@@ -0,0 +1,19 @@
+class Substraction {
+
+    public static void main(String[] args) {
+
+        int a, b;
+
+        a = 15;
+
+        b = 10;
+
+        int sub = a - b;
+
+        System.out.println("Substraction of a and b is:" + Sub);
+
+        System.out.println("---------End-----------");
+
+    }
+
+}
